8189e28896dda6a1331a8c7cedf2e5fced5fe863
AnFeliz <angel.esteban.feliz@gmail.com>
2023-10-21 10:25:44 -0400

    creating a function to calculate cramer V

--- 06 Stadistical inference/cramer-correlation.R
+++ 06 Stadistical inference/cramer-correlation.R
@@ -1,48 +1,102 @@
-# load packages
-library(data.table)
 
-get_cramer <- function(df,
-                       V1,
-                       V2,
-                       ...){
+# Internal Function
+
+cramerV_2cols <- function(df,
+                          V1,
+                          V2,
+                          ci,
+                          ...){
   
-  df <- copy(df)
+  df <- data.table::copy(df)
   
-  setnames(df, c(V1, V2), c("V1", "V2"))
+  data.table::setnames(df, c(V1, V2), c("V1", "V2"))
   
-  df[, .N, c("V1", "V2")] |>
-    dcast(V1 ~ V2, value.var = "N") |>
+  cramer_value <-
+    df[, .N, c("V1", "V2")] |>
+    data.table::dcast(V1 ~ V2, value.var = "N") |>
     (\(x) as.data.frame(x[, !c("V1")], row.names = x$V1) )() |>
     as.matrix() |>
-    rcompanion::cramerV(...)
+    rcompanion::cramerV(ci = ci, ...)
+  
+  return(cramer_value)
+  
 }
 
-get_chr_cramer <- function(df, ...){
+
+# Function to export
+
+## df: A data.table or data.frame with factor or character columns.
+## unique: As `cramerV(x,y) == cramerV(y,x)`, it avoids calculating both results.
+## ci: If TRUE, returns confidence intervals by bootstrap. May be slow.
+## ...: Additional arguments passed to `cramerV`.
+
+cramerV_df <- function(df,
+                       unique = TRUE,
+                       ci = FALSE,
+                       ...){
   
-  df <- df[, .SD, .SDcols = is.character]
+  stopifnot("df must be data.frame" = is.data.frame(df))
   
-  Comb <-
-    seq_len(ncol(df)-1) |>
-    lapply(\(x) names(df)[-(1:x)]) |>
-    mapply(FUN = \(V1,V2) CJ(V1,V2),
-           V1 = names(df) |> head(-1L),
-           V2 = _,
-           SIMPLIFY = FALSE) |>
-    rbindlist()
+  # Transforming the data.frame into a data.table
+  if(!data.table::is.data.table(df)){
+    df_class <- class(df)
+    df <- data.table::as.data.table(df) 
+  }
   
-  Comb[, .(cramer = get_cramer(df, V1, V2, ...)),
-       by = c("V1", "V2")]
+  # We just can apply the next process in data.frames with 
+  # at least 2 categorical variables
+  df <- 
+    df[, .SD, 
+       .SDcols = \(x) is.character(x) | is.factor(x)]
+  stopifnot("df must have at least 2 categorical variables" = ncol(df) >= 2L)
+  
+  # Defining combinations to calculate
+  comb <-
+    if(unique){
+      names(df) |>
+        combn(2) |> 
+        t() |>
+        data.table::as.data.table()
+    }else{
+      data.table::CJ(V1 = names(df),
+                     V2 = names(df)
+      )[V1 != V2]
+    }
+  
+  # Calculating cramer value
+  all_cramers <-
+    comb[,{
+      if(ci){
+        cramerV_2cols(df, V1, V2, ci, ...)
+      }else{
+        list(Cramer.V = rcompanion::cramerV(
+          df[[V1]], 
+          df[[V2]], 
+          ci = ci, 
+          ...
+        ) )
+      }
+    },
+    by = c("V1", "V2")]
+  
+  # Returning df to its original class
+  if(!"data.table" %in% df_class){
+    data.table::setattr(all_cramers, "class", df_class)
+  }
+  
+  return(all_cramers)
 }
 
-# create a sample data frame
-df <- data.table(
-  gender = sample(c("male", "female"), 100, replace = TRUE),
-  race = sample(c("white", "black", "asian", "other"), 100, replace = TRUE),
+# Example
+set.seed(2024)
+df <- data.frame(
+  gender = sample(c("male", "female"), 100, replace = TRUE) |> factor(),
+  race = sample(c("white", "blackr", "asian", "other"), 100, replace = TRUE),
   education = sample(c("high school", "college", "graduate"), 100, replace = TRUE),
   income = sample(c("low", "medium", "high"), 100, replace = TRUE)
 )
+set.seed(NULL)
 
-get_chr_cramer(df)
-
-
+cramerV_df(df)
+cramerV_df(df, ci = TRUE)
 
