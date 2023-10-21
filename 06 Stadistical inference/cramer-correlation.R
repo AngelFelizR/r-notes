b95e6793112b95fb344983f7ca45c097305486a0
AnFeliz <angel.esteban.feliz@gmail.com>
2023-10-21 11:05:22 -0400

    Adding pendendencies for the cramerV_df function

--- 06 Stadistical inference/cramer-correlation.R
+++ 06 Stadistical inference/cramer-correlation.R
@@ -1,5 +1,75 @@
 
-# Internal Function
+# Dependencies ----
+
+# install.packages("data.table")
+# install.packages("rcompanion")
+
+sessionInfo()
+
+# R version 4.2.3 (2023-03-15 ucrt)
+# Platform: x86_64-w64-mingw32/x64 (64-bit)
+# Running under: Windows 10 x64 (build 22621)
+# 
+# Matrix products: default
+# 
+# locale:
+# [1] LC_COLLATE=English_United States.utf8 
+# [2] LC_CTYPE=English_United States.utf8   
+# [3] LC_MONETARY=English_United States.utf8
+# [4] LC_NUMERIC=C                          
+# [5] LC_TIME=English_United States.utf8    
+# 
+# attached base packages:
+# [1] stats     graphics  grDevices
+# [4] utils     datasets  methods  
+# [7] base     
+# 
+# loaded via a namespace (and not attached):
+# [1] Rcpp_1.0.11       
+# [2] plyr_1.8.7        
+# [3] cellranger_1.1.0  
+# [4] compiler_4.2.3    
+# [5] class_7.3-21      
+# [6] tools_4.2.3       
+# [7] boot_1.3-28.1     
+# [8] rootSolve_1.8.2.4 
+# [9] lattice_0.20-45   
+# [10] Matrix_1.5-3      
+# [11] rstudioapi_0.15.0 
+# [12] parallel_4.2.3    
+# [13] mvtnorm_1.2-3     
+# [14] expm_0.999-7      
+# [15] coin_1.4-3        
+# [16] libcoin_1.0-10    
+# [17] e1071_1.7-13      
+# [18] withr_2.5.1       
+# [19] httr_1.4.7        
+# [20] DescTools_0.99.50 
+# [21] nortest_1.0-4     
+# [22] gld_2.6.6         
+# [23] stats4_4.2.3      
+# [24] lmtest_0.9-40     
+# [25] grid_4.2.3        
+# [26] data.table_1.14.8 
+# [27] R6_2.5.1          
+# [28] readxl_1.4.3      
+# [29] survival_3.5-3    
+# [30] lmom_3.0          
+# [31] multcomp_1.4-25   
+# [32] TH.data_1.1-2     
+# [33] codetools_0.2-19  
+# [34] matrixStats_1.0.0 
+# [35] modeltools_0.2-23 
+# [36] MASS_7.3-58.2     
+# [37] splines_4.2.3     
+# [38] Exact_3.2         
+# [39] multcompView_0.1-9
+# [40] sandwich_3.0-2    
+# [41] proxy_0.4-27      
+# [42] rcompanion_2.4.34 
+# [43] zoo_1.8-12   
+
+# Internal Function ----
 
 cramerV_2cols <- function(df,
                           V1,
@@ -23,12 +93,19 @@ cramerV_2cols <- function(df,
 }
 
 
-# Function to export
+# Function to export ----
+
+# Arguments
 
 ## df: A data.table or data.frame with factor or character columns.
 ## unique: As `cramerV(x,y) == cramerV(y,x)`, it avoids calculating both results.
 ## ci: If TRUE, returns confidence intervals by bootstrap. May be slow.
 ## ...: Additional arguments passed to `cramerV`.
+
+# Value
+
+## A data.frame with the `Cramer.V` for each combination of categorical variable
+## present in the original `df`
 
 cramerV_df <- function(df,
                        unique = TRUE,
